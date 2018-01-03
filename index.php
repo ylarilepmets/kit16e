e70b7a1edf3e3e8afb6ca5f7949f92a59c8fb879
unknown <ylari.lepmets@tptlive.ee>
2018-01-03 13:02:38 +0200

    Esimene commit 03.01.18

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+//String
+$tervitus = "Tere";
+//int
+$nr = 1;
+//float
+$nr1 = 10.0121;
+//boolean
+$bool = False;
+//massiiv
+$array = array('Nimi',1986,11.26,'Perenimi');
+
+/*
+mitmerealine kommentaar
+*/
+
+
+echo "$tervitus World!<br>";
+echo '$tervitus World!<br>';
+echo $array[0];
+
+ ?>
