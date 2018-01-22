e56e5e17bae58310321c4a55d2222b1fe5b2ebb8
unknown <ylari.lepmets@tptlive.ee>
2018-01-22 14:10:43 +0200

    counter jäi pooleli ja suur massiiv

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<meta charset="utf-8" />
+
 <?php
 //String
 $tervitus = "Tere";
@@ -9,7 +11,14 @@ $nr1 = 10.0121;
 $bool = False;
 //massiiv
 $array = array('Nimi',1986,11.26,'Perenimi');
-
+$esta = 'Eesti keel';
+$mata = 'Matemaatika';
+$fyss = 'Füüsika';
+//mitmetasemeline massiiv, masiivi sees on kolm massiivi(sidusmassivina)(indexid peavad olema unikaalsed)
+//nimed on reanimed ja õppeaineon tulba pealkiri kui tabelina võtta
+$hinded = array('Peeter'=>array($esta=>4, $mata=>3, $fyss=>5),
+                'Kalle'=>array($esta=>3, $mata=>4, $fyss=>5),
+                'Mari'=>array($esta=>5, $mata=>3, $fyss=>2));
 /*
 mitmerealine kommentaar
 */
@@ -25,9 +34,39 @@ if ($bool == False) {
     echo $array[$i].'<br>';
   }
   //kuvab terve masiivi sisu, ja näidatakse ära ka elementide indexid
-  Print_r($array);
+  //Print_r($array).'<br>';
 } else {
   echo "Midagi muud";
 }
+//korjab välja massivi indeksid(nimed)
+$hinded_id = array_keys($hinded);
+for ($i=0; $i < count($hinded) ; $i++) {
+   echo $hinded_id[$i].'<br>';
+   foreach ($hinded[$hinded_id[$i]] as $aine => $hinne) {
+     echo $aine.":".$hinne.'<br>';
+   }
+}
+/* counter */
+
+//anname muutujale tegevused
+$file = fopen('countlog.txt',"w") or die("Ei saa faili avada"); //"a"kirjutab sisusse juurde "w"kirjutab üle "r"loeb sisu
+if (!$file) {
+  $txt = 1;
+  fwrite($file,$txt);
+  fclose($file);
+}else {
+  $txt = fread($file, filesize('counter.txt'));
+  $new_txt = $txt++;
+  fwrite($file,$new_txt);
+  fclose($file);
+}
+
+
+
+
+
+
+
+
 
  ?>
