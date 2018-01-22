fd7f9ca986cb67555c00b1d82581806cda16a73c
unknown <ylari.lepmets@tptlive.ee>
2018-01-22 12:35:07 +0200

    lisatud fro tsükkel

--- index.php
+++ index.php
@@ -17,9 +17,14 @@ mitmerealine kommentaar
 
 echo "$tervitus World!<br>";
 echo '$tervitus World!<br>';
-echo $array[0];
+echo $array[0]. '<br>';
 
 if ($bool == False) {
+  //loetleb kõik massiivi elemendid üksteise alla, ilma indexiteta
+  for($i=0; $i < count($array); $i++){
+    echo $array[$i].'<br>';
+  }
+  //kuvab terve masiivi sisu, ja näidatakse ära ka elementide indexid
   Print_r($array);
 } else {
   echo "Midagi muud";
